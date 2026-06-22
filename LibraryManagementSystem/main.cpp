fcbeb2e59eb61a17e273d998dd60593dbf48ad6c
Mayur Gund <mgund1920@gmail.com>
2026-06-22 08:28:54 +0000

    initialized library management system

---
+++ LibraryManagementSystem/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std ;
+
+struct Student{
+    int roll_no ;
+    string name ;
+    string branch ;
+    string prn ;
+};
+
+struct Book{
+    int book_id ;
+    string book_title ;
+    string author ;
+    bool is_issued ; // true -> 1 ; false -> 0;
+}
+
+int main(){
+
+    return 0;
+}
